deae56d2da374997ef0bc8dda47959eaa09a8277
Pranjal Patil <pranjalpatil0705@gmail.com>
2026-04-11 14:48:34 +0530

    Refactor searchBooks to use axios for API calls
    Replaced GraphQL query with axios call to Google Books API for searching books.

--- src/api/graphql/queries/search-books.ts
+++ src/api/graphql/queries/search-books.ts
@@ -1,29 +1,48 @@
-import { gql } from "@apollo/client";
+import axios from "axios";
 
-export const SEARCH_GOOGLE_BOOKS_QUERY = gql`
-  query SearchGoogleBooks($query: String!, $apiKey: String!, $startIndex: Int) {
-    searchGoogleBooks(query: $query, apiKey: $apiKey, startIndex: $startIndex)
-      @rest(
-        type: "SearchBooks"
-        path: "?q={args.query}&key={args.apiKey}&startIndex={args.startIndex}"
-      ) {
-      totalItems
-      items @type(name: "GoogleBook") {
-        id
-        volumeInfo @type(name: "GoogleBookVolumeInfo") {
-          title
-          authors
-          description
-          publisher
-          publishedDate
-          imageLinks @type(name: "GoogleBookImageLinks") {
-            smallThumbnail
-            thumbnail
-          }
-          infoLink
-          previewLink
-        }
-      }
-    }
-  }
-`;
+const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
+
+export const searchGoogleBooks = async (
+  query: string,
+  apiKey: string,
+  startIndex = 0
+) => {
+  const response = await axios.get(GOOGLE_BOOKS_API, {
+    params: {
+      q: query,
+      key: apiKey,
+      startIndex,
+    },
+  });
+
+  return response.data;
+};
+// import { gql } from "@apollo/client";
+
+// export const SEARCH_GOOGLE_BOOKS_QUERY = gql`
+//   query SearchGoogleBooks($query: String!, $apiKey: String!, $startIndex: Int) {
+//     searchGoogleBooks(query: $query, apiKey: $apiKey, startIndex: $startIndex)
+//       @rest(
+//         type: "SearchBooks"
+//         path: "?q={args.query}&key={args.apiKey}&startIndex={args.startIndex}"
+//       ) {
+//       totalItems
+//       items @type(name: "GoogleBook") {
+//         id
+//         volumeInfo @type(name: "GoogleBookVolumeInfo") {
+//           title
+//           authors
+//           description
+//           publisher
+//           publishedDate
+//           imageLinks @type(name: "GoogleBookImageLinks") {
+//             smallThumbnail
+//             thumbnail
+//           }
+//           infoLink
+//           previewLink
+//         }
+//       }
+//     }
+//   }
+// `;
