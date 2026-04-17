9985e458ffda3db0b4e1f3213d48d7effd999480
Pranjal Patil <pranjalpatil0705@gmail.com>
2026-04-17 17:54:32 +0530

    Update search-books.ts

--- src/api/graphql/queries/search-books.ts
+++ src/api/graphql/queries/search-books.ts
@@ -4,54 +4,18 @@ const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
 
 export const searchGoogleBooks = async (
   query: string,
-  startIndex = 0,
-  retries = 1
+  apiKey: string,
+  startIndex = 0
 ) => {
-  try {
-    const response = await axios.get(GOOGLE_BOOKS_API, {
-      timeout: 5000,
-      params: {
-        q: query,
-        startIndex,
-        maxResults: 20,
-      },
-    });
+  const response = await axios.get(GOOGLE_BOOKS_API, {
+    params: {
+      q: query,
+      key: apiKey,
+      startIndex,
+    },
+  });
 
-    return {
-      totalItems: response.data.totalItems || 0,
-      items: response.data.items || [],
-    };
-  } catch (error: any) {
-    if (error.response?.status === 503 && retries > 0) {
-      await new Promise((res) => setTimeout(res, 1500));
-      return searchGoogleBooks(query, startIndex, retries - 1);
-    }
-
-    throw {
-      status: error.response?.status,
-      message: error.message,
-    };
-  }
+  return response.data;
 };
-
-// import axios from "axios";
-
-// const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
-
-// export const searchGoogleBooks = async (
-//   query: string,
-//   apiKey: string,
-//   startIndex = 0
-// ) => {
-//   const response = await axios.get(GOOGLE_BOOKS_API, {
-//     params: {
-//       q: query,
-//       key: apiKey,
-//       startIndex,
-//     },
-//   });
-
-//   return response.data;
-// };
 
 
