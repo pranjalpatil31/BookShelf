2fe4d7e16db64366e6b6b38497f2a5869ba3fa3a
Pranjal Patil <pranjalpatil0705@gmail.com>
2026-04-18 12:33:32 +0530

    Refactor searchGoogleBooks function to simplify parameters

--- src/api/graphql/queries/search-books.ts
+++ src/api/graphql/queries/search-books.ts
@@ -1,43 +1,21 @@
 import axios from "axios";
 
+const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
+
 export const searchGoogleBooks = async (
   query: string,
   apiKey: string,
-  startIndex: number,
-  signal?: AbortSignal // ✅ accept signal
+  startIndex = 0
 ) => {
-  const response = await axios.get(
-    "https://www.googleapis.com/books/v1/volumes",
-    {
-      params: {
-        q: query,
-        key: apiKey,
-        startIndex,
-      },
-      signal: signal, // ✅ explicitly pass it
-    }
-  );
+  const response = await axios.get(GOOGLE_BOOKS_API, {
+    params: {
+      q: query,
+      key: apiKey,
+      startIndex,
+    },
+  });
 
   return response.data;
 };
-// import axios from "axios";
-
-// const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
-
-// export const searchGoogleBooks = async (
-//   query: string,
-//   apiKey: string,
-//   startIndex = 0
-// ) => {
-//   const response = await axios.get(GOOGLE_BOOKS_API, {
-//     params: {
-//       q: query,
-//       key: apiKey,
-//       startIndex,
-//     },
-//   });
-
-//   return response.data;
-// };
 
 
